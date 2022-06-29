c7ebb7b4c006d08708fe0360bd83b8eb075451df
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-28 23:48:14 -0300

    added register and root route

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
+require('dotenv').config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    port: 5432,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,28 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 4,
-      joined: new Date(),
-    },
-  ],
-};
 app.get('/', (req, res) => {
-  res.status(200).send('all fine by now');
+  db.select('*')
+    .from('users')
+    .then((response) => {
+      res.status(200).json(response);
+    });
 });
 
 app.post('/signing', (req, res) => {
@@ -37,7 +34,20 @@ app.post('/signing', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  res.json('register');
+  const { email, name, password } = req.body;
+  db('users')
+    .returning('*')
+    .insert({
+      email,
+      name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => {
+      res.status(400).json('not able to register');
+    });
 });
 
 app.get('/profile/:id', (req, res) => {
