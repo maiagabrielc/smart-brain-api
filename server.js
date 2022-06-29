d4beaa5a1208376fd730e93449d9c1705cf12139
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-29 00:08:02 -0300

    finished /image route

--- server.js
+++ server.js
@@ -62,7 +62,15 @@ app.get('/profile/:id', (req, res) => {
 });
 
 app.put('/image', (req, res) => {
-  res.json('image');
+  const { id } = req.body;
+  db.from('users')
+    .where({ id })
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json('unable to get entries'));
 });
 
 app.listen(PORT, () => {
